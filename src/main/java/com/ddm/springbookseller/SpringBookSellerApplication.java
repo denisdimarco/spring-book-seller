689f777fa5f59cc51084061858b3a353b1884d54
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-22 23:54:28 -0300

    Project created

---
+++ src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
@@ -0,0 +1,13 @@
+package com.ddm.springbookseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddm/springbookseller/SpringBookSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddm.springbookseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
