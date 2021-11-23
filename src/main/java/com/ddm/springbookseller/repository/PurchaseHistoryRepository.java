0cf9391ab8876a6d4d2bee1c3fc870596199f1a7
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-23 03:04:40 -0300

    Refactor interface name

--- src/main/java/com/ddm/springbookseller/repository/PurchaseHistoryRepository.java
+++ src/main/java/com/ddm/springbookseller/repository/PurchaseHistoryRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PurchaseRepository extends JpaRepository<PurchaseHistory, Long> {
+public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory, Long> {
 
     @Query("select " +
             "b.title as title, ph.price as price, ph.purchaseTime as purchaseTime " +
             "from PurchaseHistory ph left join Book b on b.id = ph.bookId " +
             "where ph.userId = :userId")
-List<PurchaseItem> findAllPurchasesOfUser(@Param("userId") Long userId);
+    List<PurchaseItem> findAllPurchasesOfUser(@Param("userId") Long userId);
 
 }
