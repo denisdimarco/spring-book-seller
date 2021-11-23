ac83a8ce9e3caa8b23b3dee9d41db66ae68c878c
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-23 16:28:29 -0300

    Security Implementation, Configured Security

--- src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
+++ src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
@@ -2,18 +2,12 @@ package com.ddm.springbookseller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
 public class SpringBookSellerApplication {
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBookSellerApplication.class, args);
