3577f62c2ede886633eb0f415c4612ffc4ef5128
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-25 02:04:08 -0300

    PurchaseHistoryController implemented

---
+++ src/main/java/com/ddm/springbookseller/controller/PurchaseHistoryController.java
@@ -0,0 +1,32 @@
+package com.ddm.springbookseller.controller;
+
+
+import com.ddm.springbookseller.model.PurchaseHistory;
+import com.ddm.springbookseller.security.UserPrincipal;
+import com.ddm.springbookseller.service.PurchaseHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/purchase-history")
+public class PurchaseHistoryController {
+
+    @Autowired
+    private PurchaseHistoryService purchaseHistoryService;
+
+    @PostMapping
+    public ResponseEntity<?> savePurchaseHistory(@RequestBody PurchaseHistory purchaseHistory) {
+        return new ResponseEntity<>(purchaseHistoryService.savePurchaseHistory(purchaseHistory), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<?> getAllPurchasesOfUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        return ResponseEntity.ok(purchaseHistoryService.findPurchasedItemsOfUser(userPrincipal.getId()));
+    }
+}
