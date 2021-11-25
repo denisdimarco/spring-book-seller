8eadc16e54c9ae1073c2386a641bfee80c215a59
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-25 12:54:40 -0300

    Added mainClassName to gradle and @PropertySource to MainClass

--- src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
+++ src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
@@ -2,9 +2,11 @@ package com.ddm.springbookseller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class SpringBookSellerApplication {
 
 
