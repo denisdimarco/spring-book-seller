795c2e59e5dcff00a2a678334d60c62a62ab9319
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-30 23:31:59 -0300

    added files for production

--- src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
+++ src/main/java/com/ddm/springbookseller/SpringBookSellerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBookSellerApplication {
 
 
