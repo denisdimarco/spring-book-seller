14e503576bb14a06243b4aeb36029a042c400887
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-30 02:15:36 -0300

    Update PurchaseHistoryController.java

--- src/main/java/com/ddm/springbookseller/controller/PurchaseHistoryController.java
+++ src/main/java/com/ddm/springbookseller/controller/PurchaseHistoryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class PurchaseHistoryController {
         return new ResponseEntity<>(purchaseHistoryService.savePurchaseHistory(purchaseHistory), HttpStatus.CREATED);
     }
 
+    @GetMapping
     public ResponseEntity<?> getAllPurchasesOfUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
         return ResponseEntity.ok(purchaseHistoryService.findPurchasedItemsOfUser(userPrincipal.getId()));
